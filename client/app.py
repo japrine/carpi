b47e3178d41a3e7c97aa2e3840d72222a692ec49
japrine <jprine01@gmail.com>
2019-02-08 12:17:28 -0600

    Update

--- client/app.py
+++ client/app.py
@@ -2,6 +2,7 @@ import requests
 import json
 import sched
 import time
+import os               # for sensors
 
 
 with open('config.json', 'r') as f:
@@ -21,6 +22,29 @@ DATA = {
 }
 
 
+def get_temp_sensors():
+    ds18b20 = []
+    try:
+        for i in os.listdir('/sys/bus/w1/devices'):
+            if i != 'w1_bus_master1':
+                ds18b20.append(i)
+    except FileNotFoundError:
+        print('No Temp Sensors')
+    return ds18b20
+
+
+def read_sensors(ds18b20):
+    location = '/sys/bus/w1/devices/' + ds18b20 + '/w1_slave'
+    with open(location) as f:
+        text = f.read()
+    second = text.split("\n")[1]
+    temp_data = second.split(" ")[9]
+    temperature = float(temp_data[2:])
+    celsius = temperature / 1000
+    farenheit = (celsius * 1.8) + 32
+    return celsius, farenheit
+
+
 def update_status(sc):
     s.enter(60, 1, update_status, (sc,))                                            # Run every 60sec
     try:
@@ -35,6 +59,9 @@ def update_status(sc):
         else:
             r.raise_for_status()
 
+
+sensor_list = get_temp_sensors()
+print(sensor_list)
 
 s.enter(60, 1, update_status, (s,))
 s.run()
